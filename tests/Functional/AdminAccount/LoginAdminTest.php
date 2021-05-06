8a55739ddbb25827a0128887c94e0ed9b79b953c
SylvainG <sguerrin549@gmail.com>
2021-05-06 10:15:02 +0200

    Add admin account

---
+++ tests/Functional/AdminAccount/LoginAdminTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\UserAccount;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+/**
+ * Class LoginTest.
+ */
+class LoginAdminTest extends WebTestCase
+{
+    public function testIfLoginSuccessfully(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $urlGenerator->generate('security_admin_login'));
+//        $crawler = $client->request('GET', '/login');
+        $form = $crawler->filter('form[name=login]')->form([
+            'email' => 'ain0@email.com',
+            'password' => 'password0',
+        ]);
+
+        $client->submit($form);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        self::assertRouteSame('account_admin_profile');
+    }
+
+    public function testIfEmailDoesNotExist(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $urlGenerator->generate('security_admin_login'));
+
+//        $crawler = $client->request('GET', '/login');
+        $form = $crawler->filter('form[name=login]')->form([
+            'email' => 'fail@email.com',
+            'password' => 'password0',
+        ]);
+
+        $client->submit($form);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        self::assertRouteSame('security_admin_login');
+    }
+
+    public function testIfPasswordIsWrong(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $urlGenerator->generate('security_admin_login'));
+
+//        $crawler = $client->request('GET', '/login');
+        $form = $crawler->filter('form[name=login]')->form([
+            'email' => 'ain0@email.com',
+            'password' => 'pass',
+        ]);
+
+        $client->submit($form);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        self::assertRouteSame('security_admin_login');
+    }
+
+    public function testIfWrongCsrfToken(): void
+    {
+        $client = static::createClient();
+
+        /** @var UrlGeneratorInterface $urlGenerator */
+        $urlGenerator = $client->getContainer()->get('router');
+
+        $crawler = $client->request('GET', $urlGenerator->generate('security_login'));
+
+//        $crawler = $client->request('GET', '/login');
+        $form = $crawler->filter('form[name=login]')->form([
+            'email' => 'ain0@email.com',
+            'password' => 'password0',
+            '_csrf_token' => 'fail',
+        ]);
+
+        $client->submit($form);
+
+        self::assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        self::assertRouteSame('security_login');
+    }
+}
