1b57d6fe5a4690bb72852cdcebafb947ec13ef68
SylvainG <sguerrin549@gmail.com>
2022-01-20 13:22:37 +0100

    add stats on admin dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
+use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,13 +27,52 @@ class DashboardController extends AbstractController
 
     /**
      * @Route ("/dashboard", name="admin_dashboard")
+     * @throws \Exception
      */
-    public function dashboard(Request $request): Response
-    {
+    public function dashboard(
+        Request $request,
+        PostRepository $postRepository,
+        UserRepository $userRepository,
+        CommentRepository $commentRepository
+    ): Response {
+        $veilles = $postRepository->findAll();
+        $countUnvalidatedVeilles = 0;
+        //dump($veilles);
+        foreach ($veilles as $veille) {
+            if ($veille->isValidatedAt() === false) {
+                $countUnvalidatedVeilles++;
+            }
+        }
+        $countUnaprovedComments = count($commentRepository->findAllUnapproved());
+        $users = $userRepository->findAll();
+        $unvalidatedUsers = 0;
+        $countContributors = 0;
+        $countUnvalidatedContributors = 0;
+        foreach ($users as $user) {
+            if ($user->isActivate() === false) {
+                $unvalidatedUsers++;
+            }
+            if ($user->getRoles()[0] === 'ROLE_CONTRIBUTOR') {
+                $countContributors++;
+            }
+            //dump(isset($user->getRequests()[0]));
+//            if ($user->getRequests() !== null) {
+            if ($user->getRequests() !== null && isset($user->getRequests()["contributor"])) {
+                if ($user->getRequests()["contributor"] === "requesting") {
+                    $countUnvalidatedContributors++;
+                }
+                //dump($user->getRequests()["contributor"]);
+            }
+        }
 
         return $this->render('admin/dashboard.html.twig', [
-//            'form' => $form->createView(),
-//            'avatar' => $avatar,
+            'countVeilles' => count($veilles),
+            'countUnvalidatedVeilles' => $countUnvalidatedVeilles,
+            'countUnaprovedComments' => $countUnaprovedComments,
+            'countUsers' => count($users),
+            'countUnvalidatedUsers' => $unvalidatedUsers,
+            'countContributors' => $countContributors,
+            'countUnvalidatedContributors' => $countUnvalidatedContributors,
         ]);
     }
 }
