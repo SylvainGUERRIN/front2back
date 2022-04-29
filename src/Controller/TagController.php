54a5be738fa4c916e308302df1a100c95a5ee63b
SylvainG <sguerrin549@gmail.com>
2022-04-29 17:47:07 +0200

    add posts from tag view

--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -9,6 +9,7 @@ use App\Services\TagStatsManager;
 use App\Services\UserStatsManager;
 use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -32,37 +33,47 @@ class TagController extends AbstractController
         UserStatsManager $userStatsManager,
         ManagerRegistry $managerRegistry
     ) {
-        $this->postRepository = $postRepository;
+//        $this->postRepository = $postRepository;
         $this->tagRepository = $tagRepository;
         $this->request = $request;
-        $this->tagStatsManager = $tagStatsManager;
-        $this->userStatsManager = $userStatsManager;
-        $this->doctrine = $managerRegistry;
+//        $this->tagStatsManager = $tagStatsManager;
+//        $this->userStatsManager = $userStatsManager;
+//        $this->doctrine = $managerRegistry;
     }
 
     /**
      * @param $slug
      * @param Request $request
-     * @param FavoriteRepository $favoriteRepository
+//     * @param FavoriteRepository $favoriteRepository
      * @return Response
      * @Route("/tags/{slug}", name="tag_show")
+     * @throws \Exception
      */
     public function show(
         $slug,
         Request $request,
-        FavoriteRepository $favoriteRepository
+        PaginatorInterface $paginator
+//        FavoriteRepository $favoriteRepository
     ): Response {
         //do favorite logic
 
         //get tag
         $tag = $this->tagRepository->findBySlug($slug);
 
-        //get posts from tag
-        $posts = $tag->getPosts();
+        //get posts from tag with pagination
+        //$posts = $tag->getPosts();
+        $elementPerPage = 6;
+        $posts = $paginator->paginate(
+            $tag->getPosts(),
+            $this->request->getCurrentRequest()->query->getInt('page', 1),
+            $elementPerPage
+        );
+        $pageCount = ceil($posts->getTotalItemCount() / $elementPerPage);
 
         return $this->render('tag/show.html.twig', [
             'tag' => $tag,
             'posts' => $posts,
+            'pageCount' => $pageCount
         ]);
     }
 }
